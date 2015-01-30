fa95f2843a2bcb0f6c127109fed558af8b33bacb
ayoubm <ayoub.maat@gmail.com>
2015-01-30 10:26:13 +0100

    removed google maps from index.html and beautified js files

---
+++ acknowledge.php
@@ -0,0 +1,32 @@
+<?php
+if (isset($_POST['submit'])) {
+    $to = 'ayoub.maat@gmail.com'; // Use your own email address
+    $subject = 'Feedback from my site';
+    $message = 'Name: ' . $_POST['cf_name'] . "\r\n\r\n";
+    $message .= 'Email: ' . $_POST['cf_email'] . "\r\n\r\n";
+    $message .= 'Comments: ' . $_POST['cf_message'];
+
+    $headers = "From: ayoub@ayoubm.com\r\n";
+    $headers .= 'Content-Type: text/plain; charset=utf-8';
+
+    $email = filter_input(INPUT_POST, 'cf_email', FILTER_VALIDATE_EMAIL);
+    if ($email) {
+        $headers .= "\r\nReply-To: $email";
+    }
+
+    $success = mail($to, $subject, $message, $headers);
+
+    //header('Location: thank-you.html');
+}
+
+?>
+
+<body>
+    <?php if (isset($success) && $success) { ?>
+    <h1>Thank You</h1>
+        <p>Your message has been sent.</p>
+    <?php } else { ?>
+    <h1>Oops!</h1>
+        <p>Sorry, there was a problem sending your message.</p>
+    <?php } ?>
+</body>
